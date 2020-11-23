0e0e878d5397c4c1f3ef8daf5657788a01f2d621
Juan Jose Jauregui Acosta <jjaureguia@hmujer.saludtab.gob.mx>
2020-11-23 10:47:22 -0600

    formulario

--- resources/views/consulta_externa/consulta_externa.blade.php
+++ resources/views/consulta_externa/consulta_externa.blade.php
@@ -1,3 +1,18 @@
 <?php
+require('..\layouts\app.blade.php');
 ?>
-<h1>hola</h1>
+
+<form>
+  <div class="form-group">
+    <label for="name">Nombre</label>
+    <input type="name" class="form-control" id="name" >
+  </div>
+  <div class="form-group">
+    <label for="lastname">Apellido Paterno</label>
+    <input type="lastname" class="form-control" id="lastname" >
+  </div>
+  <div class="form-group">
+    <label for="secondlastname">Apellido Materno</label>
+    <input type="secondlastname" class="form-control" id="secondlastname" >
+  </div>
+</form>
