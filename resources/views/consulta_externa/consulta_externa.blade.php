3872f5cf079b465f90c6a561c3e35d7c9a97cacf
Juan Jose Jauregui Acosta <jjaureguia@hmujer.saludtab.gob.mx>
2020-11-25 11:27:02 -0600

    formulario 2

--- resources/views/consulta_externa/consulta_externa.blade.php
+++ resources/views/consulta_externa/consulta_externa.blade.php
@@ -47,7 +47,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm">
-            |   <div class="form-group">
+               <div class="form-group">
                     <label for="example-date-input" class="form-control-label">Nacio Extranjero</label><br/>
                     <label class="custom-toggle">
                     <input type="checkbox" checked>
@@ -86,144 +86,237 @@
         </div>
     </div>
     <div class="container">
-    <div class="row">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="numero_afiliacion" class="form-control-label">Numero de Afiliciacion</label>
+                    <input class="form-control" type="text"  id="numero_afiliacion">
+                </div>
+            </div>
     <div class="col-sm">
-    <div class="form-group">
-        <label for="numero_afiliacion" class="form-control-label">Numero de Afiliciacion</label>
-        <input class="form-control" type="text"  id="numero_afiliacion">
+        <div class="form-group">
+                     <label for="numero_afiliacion" class="form-control-label">Afiliacion</label>
+                     <select class="form-control form-control-sm">
+                     </select>
+                </div>
+            </div>
+    <div class="col-sm">
+        <div class="form-group">
+                    <label for="digito_verificador" class="form-control-label">Digito Verificador</label>
+                    <input class="form-control" type="number"  id="digito_verificador">
+                </div>  
+             </div>
+        </div>
     </div>
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                     <label for="example-date-input" class="form-control-label">Lee</label><br/>
+                     <label class="custom-toggle">
+                     <input type="checkbox" checked>
+                     <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
+                </label>
+            </div>
+      </div>
+    <div class="col-sm">
+        <div class="form-group">
+                        <label for="example-date-input" class="form-control-label">Escribe</label><br/>
+                        <label class="custom-toggle">
+                        <input type="checkbox" checked>
+                        <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
+                    </label>
+                </div>
+            </div>
+    <div class="col-sm">
+            <div class="form-group">
+                <label for="example-date-input" class="form-control-label">Habla Lengua Indigena</label><br/>
+                <label class="custom-toggle">
+                <input type="checkbox" checked>
+                <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
+            </label>
+        </div>
     </div>
     <div class="col-sm">
-    <div class="form-group">
-    <label for="numero_afiliacion" class="form-control-label">Afiliacion</label>
-    <select class="form-control form-control-sm">
-    </select>
+         <div class="form-group">
+                        <label for="asentamiento" class="form-control-label">Cual Lengua</label>
+                        <select class="form-control form-control-sm">
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                        <label for="example-date-input" class="form-control-label">Tiene Alguna Incapacidad</label><br/>
+                        <label class="custom-toggle">
+                    <input type="checkbox" checked>
+                <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
+            </label>
+        </div>
     </div>
+    <div class="col-sm">
+         <div class="form-group">
+                        <label for="asentamiento" class="form-control-label">Cual Discapacidad</label>
+                        <select class="form-control form-control-sm">
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="numero_expediente" class="form-control-label">Numero de Expediente</label>
+                    <input class="form-control" type="number"  id="numero_expediente">
+                </div>
+            </div>
+    <div class="col-sm">
+        <div class="form-group">
+            <label for="fecha_registro" class="form-control-label">Fecha de Registro</label>
+            <input class="form-control" type="date" id="fecha_registro">
+        </div>
     </div>
-    <div class="form-group">
-        <label for="digito_verificador" class="form-control-label">Digito Verificador</label>
-        <input class="form-control" type="number"  id="digito_verificador">
+    <div class="col-sm">
+        <div class="form-group">
+                    <label for="telefono" class="form-control-label">Telefono</label>
+                    <input class="form-control" type="tel" id="telefono">
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-    <label for="example-date-input" class="form-control-label">Lee</label><br/>
-    <label class="custom-toggle">
-    <input type="checkbox" checked>
-    <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
-    </label>
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="example-date-input" class="form-control-label">Reside en el Extranjero</label><br/>
+                    <label class="custom-toggle">
+                    <input type="checkbox" checked>
+                    <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
+                </label>
+            </div> 
+        </div> 
+    <div class="col-sm">
+        <div class="form-group">
+            <label for="entidad_residencia" class="form-control-label">Entidad Residencia</label>
+            <input class="form-control" type="text"  id="entidad_residencia">
+        </div>
     </div>
-    <div class="form-group">
-    <label for="example-date-input" class="form-control-label">Escribe</label><br/>
-    <label class="custom-toggle">
-    <input type="checkbox" checked>
-    <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
-    </label>
-    </div>
-    <div class="form-group">
-    <label for="example-date-input" class="form-control-label">Habla Lengua Indigena</label><br/>
-    <label class="custom-toggle">
-    <input type="checkbox" checked>
-    <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
-    </label>
-    </div>
-    <div class="form-group">
-    <label for="asentamiento" class="form-control-label">Cual Lengua</label>
-    <select class="form-control form-control-sm">
-    </select>
-    </div>
-    <div class="form-group">
-    <label for="example-date-input" class="form-control-label">Tiene Alguna Incapacidad</label><br/>
-    <label class="custom-toggle">
-    <input type="checkbox" checked>
-    <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
-    </label>
-    </div>
-    <div class="form-group">
-        <label for="numero_expediente" class="form-control-label">Numero de Expediente</label>
-        <input class="form-control" type="number"  id="numero_expediente">
-    </div>
-    <div class="form-group">
-        <label for="fecha_registro" class="form-control-label">Fecha de Registro</label>
-        <input class="form-control" type="date" id="fecha_registro">
-    </div>
-    <div class="form-group">
-        <label for="telefono" class="form-control-label">Telefono</label>
-        <input class="form-control" type="tel" id="telefono">
-    </div>
-    <div class="form-group">
-    <label for="example-date-input" class="form-control-label">Reside en el Extranjero</label><br/>
-    <label class="custom-toggle">
-    <input type="checkbox" checked>
-    <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
-    </label>
+    <div class="col-sm">
+        <div class="form-group">
+                    <label for="municipio_residencia" class="form-control-label">Municipio Residencia</label>
+                    <input class="form-control" type="text"  id="municipio_residencia">
+                </div>
+            </div> 
+        </div>
     </div> 
-    <div class="form-group">
-        <label for="entidad_residencia" class="form-control-label">Entidad Residencia</label>
-        <input class="form-control" type="text"  id="entidad_residencia">
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="localidad_residencia" class="form-control-label">Localidad Residencia</label>
+                    <input class="form-control" type="text"  id="localidad_residencia">
+            </div>
+        </div>
+    <div class="col-sm">
+        <div class="form-group">
+            <label for="otra_localidad" class="form-control-label">Otra Localidad</label>
+            <input class="form-control" type="text"  id="otra_localidad">
+        </div>
     </div>
-    <div class="form-group">
-        <label for="municipio_residencia" class="form-control-label">Municipio Residencia</label>
-        <input class="form-control" type="text"  id="municipio_residencia">
+    <div class="col-sm">
+        <div class="form-group">
+                    <label for="codigo_postal" class="form-control-label">Codigo Postal</label>
+                    <input class="form-control" type="text"  id="codigo_postal">
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="localidad_residencia" class="form-control-label">Localidad Residencia</label>
-        <input class="form-control" type="text"  id="localidad_residencia">
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="example-date-input" class="form-control-label">Se Ignora Codigo Postal</label><br/>
+                    <label class="custom-toggle">
+                    <input type="checkbox" checked>
+                    <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
+                </label>
+            </div>
+        </div>
+    <div class="col-sm">
+        <div class="form-group">
+            <label for="vialidad" class="form-control-label">Vialidad</label>
+            <input class="form-control" type="text"  id="vialidad">
+        </div>
     </div>
-    <div class="form-group">
-        <label for="otra_localidad" class="form-control-label">Otra Localidad</label>
-        <input class="form-control" type="text"  id="otra_localidad">
+    <div class="col-sm">
+        <div class="form-group">
+                     <label for="vialidad" class="form-control-label">Tipo Vialidad</label>
+                     <select class="form-control form-control-sm">
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="codigo_postal" class="form-control-label">Codigo Postal</label>
-        <input class="form-control" type="text"  id="codigo_postal">
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="numero_exterior" class="form-control-label">Numero Exterior</label>
+                    <input class="form-control" type="number"  id="numero_exterior">
+                </div>
+            </div>
+    <div class="col-sm">
+        <div class="form-group">
+            <label for="numero_exterior_alfabetico" class="form-control-label">Numero Exterior Alfabetico</label>
+            <input class="form-control" type="text"  id="numero_exterior_alfabetico">
+        </div>
     </div>
-    <div class="form-group">
-    <label for="example-date-input" class="form-control-label">Se Ignora Codigo Postal</label><br/>
-    <label class="custom-toggle">
-    <input type="checkbox" checked>
-    <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
-    </label>
+    <div class="col-sm">
+        <div class="form-group">
+                    <label for="numero_interior" class="form-control-label">Numero Interior</label>
+                    <input class="form-control" type="number"  id="numero_interior">
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="vialidad" class="form-control-label">Vialidad</label>
-        <input class="form-control" type="text"  id="vialidad">
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="asentamiento" class="form-control-label">Asentamiento</label>
+                    <input class="form-control" type="text"  id="asentamiento">
+                </div>
+            </div>
+    <div class="col-sm">
+        <div class="form-group">
+                        <label for="asentamiento" class="form-control-label">Tipo Asentamiento</label>
+                        <select class="form-control form-control-sm">
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-    <label for="vialidad" class="form-control-label">Tipo Vialidad</label>
-    <select class="form-control form-control-sm">
-    </select>
-    </div>
-    <div class="form-group">
-        <label for="numero_exterior" class="form-control-label">Numero Exterior</label>
-        <input class="form-control" type="number"  id="numero_exterior">
-    </div>
-    <div class="form-group">
-        <label for="numero_exterior_alfabetico" class="form-control-label">Numero Exterior Alfabetico</label>
-        <input class="form-control" type="text"  id="numero_exterior_alfabetico">
-    </div>
-    <div class="form-group">
-        <label for="numero_interior" class="form-control-label">Numero Interior</label>
-        <input class="form-control" type="number"  id="numero_interior">
-    </div>
-    <div class="form-group">
-        <label for="asentamiento" class="form-control-label">Asentamiento</label>
-        <input class="form-control" type="text"  id="asentamiento">
-    </div>
-    <div class="form-group">
-    <label for="asentamiento" class="form-control-label">Tipo Asentamiento</label>
-    <select class="form-control form-control-sm">
-    </select>
-    </div>
-    <div class="form-group">
-    <label for="escolaridad" class="form-control-label">Escolaridad</label>
-    <select class="form-control form-control-sm">
-    </select>
-    </div>
-    <div class="form-group">
-    <label for="sexo" class="form-control-label">Sexo</label>
-    <select class="form-control form-control-sm">
-    </select>
+    <div class="container">
+         <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="escolaridad" class="form-control-label">Escolaridad</label>
+                    <select class="form-control form-control-sm">
+                </select>
+            </div>
+        </div>
+    <div class="col-sm">
+        <div class="form-group">
+                        <label for="sexo" class="form-control-label">Sexo</label>
+                        <select class="form-control form-control-sm">
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
 </form>
         
