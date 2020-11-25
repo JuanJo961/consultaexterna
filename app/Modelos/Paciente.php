8a1bef9fed21813de2b571b6d0355df989b1ad9b
ivan de jesus montejo cajija <ivanmontejo10@gmail.com>
2020-11-25 09:30:58 -0600

    Tabla cat_orden y documento_paciente hechas

--- app/Modelos/Paciente.php
+++ app/Modelos/Paciente.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paciente extends Model
 {
+    //Relacion de uno a muchos
     public function Usuarios()
     {
         return $this->belongsTo('App/Modelos/Usuario');
+    }
+    //relacion de muchos a muchos
+    public function documentos()
+    {
+        return $this->belongsToMany('App\Modelos\Catalogos\CatOrdenDocumentoPaciente');
     }
 }
